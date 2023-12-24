9061be3b7a97c7b13fbf22a002c6906f3dc316a6
cyberadityacode <154729869+cyberadityacode@users.noreply.github.com>
2023-12-24 09:55:12 +0000

    second commit

---
+++ p2_strings.py
@@ -0,0 +1,9 @@
+print("Hello Strings Lesson 2")     
+# Program to reverse the given string
+
+given_string = input("Enter the string: ")
+
+# In this program, [::-1] is used 
+# to create a reversed copy of the input strin
+
+print(given_string[::-1])
